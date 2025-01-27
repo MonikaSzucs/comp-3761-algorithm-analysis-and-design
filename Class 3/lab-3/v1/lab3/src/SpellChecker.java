3bbe16a5424beaf94ccb44abb37137e0d9f17262
Monika Szucs <monika.szucs.work@gmail.com>
2025-01-26 16:11:50 -0800

    saving lab 3 code

--- Class 3/lab-3/v1/lab3/src/SpellChecker.java
+++ Class 3/lab-3/v1/lab3/src/SpellChecker.java
@@ -3,35 +3,71 @@ import java.util.*;
 
 public class SpellChecker {
     public static void main(String[] args) {
-        WordListReader wordListReader = new WordListReader("lab3_wordlist.txt");
+        WordListReader wordListReader = new WordListReader("lab3_wordlist.txt", true);
+        WordListReader testData = new WordListReader("lab3_testData.txt", false);
 
         // Get the word list from the WordListReader
-        List<String> wordList = wordListReader.readWordList();
-
-        // Print the first 10 words from the word list
-//        System.out.println("First 10 words from the word list:");
-//        for (int i = 0; i < Math.min(10, wordList.size()); i++) {
-//            System.out.println(wordList.get(i));
-//        }
-
+        List<String> wordList = wordListReader.getWordList();
 
         // Print the word list to verify
         System.out.println("Word List from file:");
         for (String word : wordList) {
             System.out.println(word);
         }
-        int unfoundCount = seqSearch(wordListReader);
-        System.out.println(unfoundCount);
+
+        wordSearch(wordListReader.getWordList(), testData.getWordList(), true);
+        wordSearch(wordListReader.getWordList(), testData.getWordList(), false);
+    }
+    
+    public static void wordSearch(List<String> wordList, List<String> testData, boolean sequential){
+        int count = 0;
+        long startTime = System.nanoTime();
+        if(sequential){
+            for (int i = 0; i < testData.size(); i++) { // not part of the algorithm
+                if(seqSearch(wordList,testData.get(i))){
+                    count++;
+                }
+            }
+            System.out.println("Sequential search: " + count + "\t words " + (System.nanoTime()-startTime)/1000 + " Microsecond");
+        }
+        else{
+            for (int i = 0; i < testData.size(); i++) { // not part of the algorithm
+                if(binSearch(wordList,testData.get(i))){
+                    count++;
+                }
+            }
+            System.out.println("Binary search: " + count + "\t words " + (System.nanoTime()-startTime)/1000 + " Microsecond");
+        }
     }
 
-    public static int seqSearch(WordListReader wordListReader) {
-        List<String> wordList = new ArrayList<String>();
-
-        return wordList.size();
+    public static boolean seqSearch(List<String> wordList, String testString) {
+            for (int j = 0; j < wordList.size(); j++) {
+                if(testString.equals(wordList.get(j))){
+                    return true;
+                }
+            }
+        return false;
     }
 
-    public static int binSearch() {
-        List<String> wordList = new ArrayList<String>();
-        return wordList.size();
+    public static boolean binSearch(List<String> wordList, String testString) {
+        return binarySearchRecursive(0, wordList.size()-1, testString, wordList);
+    }
+
+    private static boolean binarySearchRecursive(int startIndex, int lastIndex, String word, List<String> wordList){
+        int middleIndex = (startIndex+lastIndex) / 2;
+
+        if (lastIndex < startIndex){
+            return false;
+        }
+        if (wordList.get(middleIndex).equals(word)) {
+            return true;
+        }
+        // if string at middleIndex is less than the word (comes before in dictionary/sorted order), we know that the half after the middleIndex should have the word.
+        else if (wordList.get(middleIndex).compareTo(word) < 0) { // smaller middle index
+            return binarySearchRecursive( middleIndex+1,  lastIndex,  word, wordList);
+        }
+        else {
+            return binarySearchRecursive( startIndex,  middleIndex-1,  word, wordList);
+        }
     }
 }
--- Class 3/lab-3/v1/lab3/src/WordListReader.java
+++ Class 3/lab-3/v1/lab3/src/WordListReader.java
@@ -4,23 +4,32 @@ import java.util.*;
 
 public class WordListReader {
     private String fileName;
+    private List<String> wordList;
 
-    public WordListReader(String fileName) {
+    public WordListReader(String fileName, boolean lineSeparated) {
         this.fileName = fileName;
+        readWordList(lineSeparated);
     }
 
-    public List<String> readWordList() {
-        List<String> wordList = new ArrayList<>();
-
+    private void readWordList(boolean lineSeparated) {
+        this.wordList = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
-            while ((line = reader.readLine()) != null) {
-                wordList.add(line.trim().toLowerCase());
+            if(lineSeparated){
+                while ((line = reader.readLine()) != null) {
+                    wordList.add(line.trim().toLowerCase());
+                }
             }
+            else{
+                this.wordList = Arrays.asList(reader.readLine().toLowerCase().split(" "));
+            }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    public List<String> getWordList() {
         return wordList;
     }
 }
